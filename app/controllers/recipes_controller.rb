e787923cb40e900152ab1f53d8bc316f41125d17
Rebekah Shein <rmshein@gmail.com>
2015-11-23 16:21:12 -0500

    updated recipes show, new and edit methods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,15 +5,18 @@ class RecipesController < ApplicationController
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
 
   def update
