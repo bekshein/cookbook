411676de849b43ad9665ad5325fc4abc3b46c211
Rebekah Shein <rmshein@gmail.com>
2015-11-23 16:23:58 -0500

    updated recipes create, update and destroy methods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,6 +13,13 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe, notice: "Recipe successfully saved"
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -20,9 +27,20 @@ class RecipesController < ApplicationController
   end
 
   def update
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @recipe = Recipe.destroy(params[:id])
+
+    redirect_to root_path, notice: "Recipe successfully deleted"
+  end
 
   private
 
