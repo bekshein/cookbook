fa4fdca7f841fcbfdb166042955804de1c893aaf
Rebekah Shein <rmshein@gmail.com>
2015-11-24 00:53:50 -0500

    updated recipe model with has_many directions and ingredients associations and added validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,12 @@
 class Recipe < ActiveRecord::Base
+  
+  has_many :directions
+  has_many :ingredients
+
+
+  validates :title, :description, :image, presence: true
+
+  # paperclip gem usage
   has_attached_file :image, styles: { medium: "400x400#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
